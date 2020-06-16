6b54abb0e0c9f3f98c1327cb2bcb33b6034a3fa3
sincere_vicky <vijayhardaha@gmail.com>
2020-06-16 17:48:14 +0530

    Fixed bugs and updated packages
    - Update packages.
    - Fixed some bugs.
    - Removed the examples sections.
    - Removed Examples assets.
    - Removed Noty, masonry, Lighygallery packages.
    - Installed sweetalert for notifications
    - Removed Noty from Form validation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './parts/header/header.component';
 import { FooterComponent } from './parts/footer/footer.component';
 import { AboutComponent } from './parts/about/about.component';
-import { ExamplesComponent } from './parts/examples/examples.component';
-import { ImageBoxComponent } from './parts/examples/image-box.component';
 import { RadioInputComponent } from './parts/header/radio-input.component';
-import { NotyService } from './parts/header/noty.service';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		FooterComponent,
-		AboutComponent,
-		ExamplesComponent,
-		ImageBoxComponent,
-		RadioInputComponent,
-	],
-	imports: [
-		BrowserModule,
-		FormsModule,
-		ReactiveFormsModule,
-		HttpClientModule
-	],
-	providers: [NotyService],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    RadioInputComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
